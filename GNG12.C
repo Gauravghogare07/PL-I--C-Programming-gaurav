7faad5491f5bdd38bc6501ed5aff997b4fef7228
Gauravghogare07 <gauravghogare08@gmail.com>
2026-09-13 18:44:12 +0530

    Implement even/odd number checker
    Fix input prompt and output messages for clarity.

---
+++ GNG12.C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+
+{
+  int num;
+  printf ("enter your name"
+);
+  scanf ("%d",&num);
+  
+  if (num%2==0)
+{
+ printf("the number is even.");
+}
+ else
+{
+ printf(" The number is odd.");
+}
+return 0;
+}
